bcc60c2af7506a7e924b067fa8beb9b5003faf0f
Kamil <Kamisseq66@gmail.com>
2024-11-27 19:43:55 +0100

    mmmm

---
+++ ZADANIA EGZAMINACYJNE/tulipany/skrypt.js
@@ -0,0 +1,29 @@
+nextButton1.addEventListener("click", (e) => {
+        document.getElementById("block1").style.display = "none";
+        document.getElementById("block2").style.display = "inline-block";
+});
+
+nextButton2.addEventListener("click", (e) => {
+    document.getElementById("block2").style.display = "none";
+    document.getElementById("block3").style.display = "inline-block";
+});
+
+
+
+submitButton.addEventListener("click", (e) => {
+        const passwordField = document.querySelectorAll('input[type="password"]');
+        const password1 = passwordField[0].value;
+        const password2 = passwordField[1].value
+
+    if (password1 !== password2) {
+        alert("Podane hasła różnią się");
+        return; 
+    }
+
+        const nameField = document.querySelectorAll('input[type="text"]');
+        const firstName = nameField[0].value;
+        const lastName = nameField[1].value;
+
+        console.log(`Witaj ${firstName} ${lastName}`);
+
+});
